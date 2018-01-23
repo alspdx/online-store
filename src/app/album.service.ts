03d74ef07a4a728a4bf48ac94fa7af2db745ac34
Guest User <Guest@Epicodus-4B.local>
2018-01-23 10:47:53 -0800

    Lena's runthrough

---
+++ src/app/album.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
+
+@Injectable()
+export class AlbumService {
+
+  constructor() { }
+
+  getAlbums() {
+    return ALBUMS;
+  }
+
+  getAlbumById(albumId: number) {
+    for (let album of ALBUMS) {
+      if (album.id === albumId) {
+        return album;
+      }
+    }
+  }
+}
